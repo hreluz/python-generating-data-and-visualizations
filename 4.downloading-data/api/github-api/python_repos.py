c2295b76c23cc98780b16ff9cc814ab88c42b0fe
Hector <hmreluz@gmail.com>
2023-01-08 11:20:55 -0500

    The hacker news api

--- 4.downloading-data/api/github-api/python_repos.py
+++ 4.downloading-data/api/github-api/python_repos.py
@@ -2,7 +2,7 @@ import requests
 
 # Make an API call and store the response
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
-headers = {'Accept': 'application/vnd.github.v3+json'}
+headers = {'Accept': 'application/vnd.github-api.v3+json'}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 # Store API response in a variable
