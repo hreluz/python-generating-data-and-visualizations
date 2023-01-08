dbda80b9be58bb2ffb7677c52bc03ead8bf6a92a
Hector <hmreluz@gmail.com>
2023-01-08 08:27:02 -0500

    Shading an area in the chart

--- 4.downloading-data/sitka_highs.py
+++ 4.downloading-data/sitka_highs.py
@@ -24,8 +24,9 @@ with open(filename) as f:
 # Plot the high temperatures
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red')
-ax.plot(dates, lows, c='blue')
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot
 # ax.set_title("Daily high temperatures, July 2018", fontsize=24)
