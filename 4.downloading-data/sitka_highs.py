fdb1b4499b7d0237d8336fb515e3e21a49677eb4
Hector <hmreluz@gmail.com>
2023-01-07 21:26:07 -0500

    Plotting a second data series

--- 4.downloading-data/sitka_highs.py
+++ 4.downloading-data/sitka_highs.py
@@ -12,21 +12,24 @@ with open(filename) as f:
     header_row = next(reader)
 
     # Get dates, and high temperatures from this file
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5] if row[5] else 0)
+        low = int(row[6] if row[6] else 0)
         dates.append(current_date)
         highs.append(high)
+        lows.append(low)
 
 # Plot the high temperatures
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
+ax.plot(dates, lows, c='blue')
 
 # Format plot
 # ax.set_title("Daily high temperatures, July 2018", fontsize=24)
-ax.set_title("Daily high temperatures - 2018", fontsize=24)
+ax.set_title("Daily high and low temperatures - 2018", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 # Creates the date labels diagonally to prevent overlappint
 fig.autofmt_xdate()
