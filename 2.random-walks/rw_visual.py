9c145ed966eb1d8ed0430dd622519681acec1d49
Hector <hmreluz@gmail.com>
2023-01-07 19:43:03 -0500

    Adding higher plot points

--- 2.random-walks/rw_visual.py
+++ 2.random-walks/rw_visual.py
@@ -5,7 +5,7 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active
 while True:
     # Make a random walks
-    rw = RandomWalk()
+    rw = RandomWalk(50_0000)
     rw.fill_walk()
 
     # Plot the points in the walk
@@ -13,15 +13,15 @@ while True:
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers,
-               cmap=plt.cm.Blues, edgecolor='none', s=15)
+               cmap=plt.cm.Blues, edgecolor='none', s=1)
 
     # Emphasize the first and last points
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 
     # Remove the axes
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+    # ax.get_xaxis().set_visible(False)
+    # ax.get_yaxis().set_visible(False)
 
     plt.show()
 
