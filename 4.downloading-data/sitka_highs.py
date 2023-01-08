2252a8ce891f45dc6e2a303f269ebc820a1c88c4
Hector <hmreluz@gmail.com>
2023-01-07 21:18:06 -0500

    Plotting dates

--- 4.downloading-data/sitka_highs.py
+++ 4.downloading-data/sitka_highs.py
@@ -1,5 +1,7 @@
 import csv
 
+from datetime import datetime
+
 import matplotlib.pyplot as plt
 
 filename = 'data/sitka_weather_07-2018_simple.csv'
@@ -8,20 +10,24 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    # Get high temperatures from this file
-    highs = []
+    # Get dates, and high temperatures from this file
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        dates.append(current_date)
         highs.append(high)
 
 # Plot the high temperatures
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 
 # Format plot
 ax.set_title("Daily high temperatures, July 2018", fontsize=24)
 ax.set_xlabel('', fontsize=16)
+# Creates the date labels diagonally to prevent overlappint
+fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(axis='both', which='major', labelsize=16)
 
